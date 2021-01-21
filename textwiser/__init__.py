e4be1e649a30be80702e01d090970a5ba055ba6c
Doruk Kilitcioglu <doruk.kilitcioglu@fmr.com>
2021-01-21 15:05:00 -0500

    Bump version
    Signed-off-by: Doruk Kilitcioglu <doruk.kilitcioglu@fmr.com>

--- textwiser/__init__.py
+++ textwiser/__init__.py
@@ -6,7 +6,7 @@
 """
 :Author: FMR LLC
 :Email: opensource@fidelity.com
-:Version: 1.3.0 of Jul 22, 2020
+:Version: 1.3.1 of Jan 21, 2021
 
 This module defines the public interface of the
 **TextWiser Library** providing access to the following modules:
